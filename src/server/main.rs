e64154c0c9defa2b85518f3431ac022457abfdce
Barret Rennie <barret@brennie.ca>
2018-06-04 01:58:37 -0600

    Add a basic CLI for the server with StructOpt

--- src/server/main.rs
+++ src/server/main.rs
@@ -1,3 +1,35 @@
+extern crate structopt;
+#[macro_use]
+extern crate structopt_derive;
+
+use std::net::IpAddr;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "server")]
+/// A simple chat server.
+struct Options {
+    /// The IP address to listen on.
+    #[structopt(
+        short = "h",
+        long = "host",
+        default_value = "127.0.0.1",
+        env = "CHAT_HOST",
+        parse(try_from_str)
+    )]
+    host: IpAddr,
+
+    /// The port to bind to.
+    #[structopt(short = "p", long = "port", default_value = "9999", env = "CHAT_PORT")]
+    port: u16,
+
+    /// The level of verbosity for logging. You may specify this more than once.
+    #[structopt(short = "v", parse(from_occurrences))]
+    verbosity: u8,
+}
+
 fn main() {
-    unimplemented!()
+    let options = Options::from_args();
+
+    println!("{:?}", options);
 }
